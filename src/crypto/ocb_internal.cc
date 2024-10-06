8500335ca0100b2231172c0231d066d4a6696c00
cynix <cynix@cynix.org>
2024-10-06 17:42:44 +1100

    Apply FreeBSD patch

--- src/crypto/ocb_internal.cc
+++ src/crypto/ocb_internal.cc
@@ -132,10 +132,10 @@
 
 #if _MSC_VER
 	#define bswap64(x) _byteswap_uint64(x)
-#elif HAVE_DECL_BSWAP64
-	/* nothing */
 #elif HAVE_DECL___BUILTIN_BSWAP64
 	#define bswap64(x) __builtin_bswap64(x)           /* GCC 4.3+ */
+#elif HAVE_DECL_BSWAP64
+	/* nothing */
 #else
 	#define bswap32(x)                                              \
 	   ((((x) & 0xff000000u) >> 24) | (((x) & 0x00ff0000u) >>  8) | \
